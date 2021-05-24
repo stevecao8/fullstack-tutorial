5e5f20c1399cd7c3e17f6a7d48a842150387b52d
Steve Cao <steve.cao@doordash.com>
2021-05-23 21:08:43 -0700

    Added login feature with local storage

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,16 +1,44 @@
 import {
-    ApolloClient, ApolloProvider, NormalizedCacheObject
+    ApolloClient,
+    ApolloProvider,
+    gql,
+    NormalizedCacheObject,
+    useQuery
 } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
 import { cache } from "./cache";
 import Pages from "./pages";
+import Login from "./pages/login";
 import injectStyles from "./styles";
+
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+`;
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 // Initialize ApolloClient
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   uri: "http://localhost:4000/graphql",
+
+  headers: {
+    authorization: localStorage.getItem("token") || "",
+  },
+  typeDefs,
 });
 
 injectStyles();
@@ -18,7 +46,7 @@ injectStyles();
 // Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
